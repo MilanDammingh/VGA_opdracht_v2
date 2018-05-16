09bde2051d29e096def9a287daa53e09773f6198
Samuel Benhaiem <samuelbenhaiem@gmail.com>
2018-05-16 14:18:20 +0200

    Push test

--- Libraries/API-LIB/Source/rectangle.c
+++ Libraries/API-LIB/Source/rectangle.c
@@ -6,10 +6,12 @@
  */
 
 #include "rectangle.h"
-#include "line.c"
+#include "line.h"
 
 void draw_rectangle (int x1, int y1, int x2, int y2, byte color)
 {
+	x1 = x1 - 1;
+	x2 = x2 + 10;
 	draw_line(x1, y1, x2, y1, color);
 	draw_line(x1, y1, x1, y2, color);
 	draw_line(x2, y2, x2, y1, color);
