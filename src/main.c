80ef5a00b10e4112f861948f58716d8045bee3bc
Samuel Benhaiem <samuelbenhaiem@gmail.com>
2018-05-16 16:51:47 +0200

    Bitmap op scherm proberen

--- src/main.c
+++ src/main.c
@@ -25,7 +25,7 @@ int main(void)
 	UB_VGA_Screen_Init(); // Init VGA-Screen
 
 	UB_VGA_FillScreen(VGA_COL_BLACK);
-	draw_rectangle(10,10,50,50,200);
+	draw_bitmap ();
 
 	/*
 	UB_VGA_SetPixel(10,10,127);
