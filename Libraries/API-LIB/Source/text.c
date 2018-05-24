215b0164af3884c15c7727c3e2c67d0331c46870
Samuel Benhaiem <samuelbenhaiem@gmail.com>
2018-05-24 09:43:15 +0200

    Bimtap en text edits

--- Libraries/API-LIB/Source/text.c
+++ Libraries/API-LIB/Source/text.c
@@ -9,7 +9,7 @@
 
 
 
-void draw_text (int x, int y, char text[], int backcolor, int textcolor) { //functie voor het schrijven van tekst
+void draw_text (int x, int y, char text, int backcolor, int textcolor) { //functie voor het schrijven van tekst
 
 	for(int i=0;i<strlen(text);i++) //uit de "text" string individuele letters en kleuren halen
 	{
